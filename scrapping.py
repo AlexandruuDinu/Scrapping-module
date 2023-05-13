3df5de2176cbc712341cc73d081a865d819f209a
Alexandruu Dinu <AlexandruGabriel.Dinu@endava.com>
2023-05-13 16:05:07 +0300

    rabbitMQ queue adding

--- scrapping.py
+++ scrapping.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 import json
 import argparse
+import pika
 
 def get_response_from_url(url):
     return requests.get(url)
@@ -13,14 +14,8 @@ def get_soup(response):
 def get_html_class(soup):
     return soup.find(attrs={'class': 'svelte-13ozom4'})
 
-def get_news_url(soup):
-    return soup.find_all('div', attrs={'class': 'svelte-13ozom4'})
-
 def get_news_child(soup):
     return soup.find_all('a', attrs={'class': 'svelte-13ozom4'})
-
-def get_news_descriptions(soup):
-    return soup.find('main', attrs={'class': 'svelte-1m9guhq'})
 
 def get_news_descriptions_child(soup):
     temp = soup.find_all('p')
@@ -62,43 +57,77 @@ def get_list_of_comments(soup):
 
 def create_comment_list(comment_data):
     comments = []
+    users = []
+    comment_users = []
     for comment in comment_data['comments']:
-        comments.append(comment['text'])
+        family_name = '"commenterName":"' + comment['user']['family_name'] + '"'
+        comment_users.append(comment['user']['family_name'])
+        comment_text = '"comment": "' + comment['text'] + '"'
+        comment_users.append(comment['text'])
+        users.append(comment_users)  
+        comment_users = []  
+    comments.append(users)
     return comments
 
-def get_description_of_news(news_list):
-    i = 0
-    rabbitMQ_list_2 = []
-
+def get_description_of_news(news_list, param):
+    # i = 0
+    rabbitMQ_list_2 = []    
     for news in news_list:
-        # comment part 
-        comment_id = re.search(r"[0-9]{7}", news).group(0)
-        comment_response = get_response_from_url(create_comment_url(comment_id))
-        comment_soup = get_soup(comment_response)
-        comment_data = json.loads(str(comment_soup)) # deserializare
-        comment_list = create_comment_list(comment_data) # list of comments
-
         # news description part   
         response = get_response_from_url(news)
         soup = get_soup(response)
         news_description = get_news_descriptions_child(soup)
 
-        # rabbitmq list part
-        rabbitMQ_list_2.append([news_description, comment_list])
-        if i == 1:
-            break
-        i += 1
-    # print(rabbitMQ_list_2)
-    return rabbitMQ_list_2
+        if type(param) == str:
+            # comment part 
+            comment_id = re.search(r"[0-9]{7}", news).group(0)
+            comment_response = get_response_from_url(create_comment_url(comment_id))
+            comment_soup = get_soup(comment_response)
+            comment_data = json.loads(str(comment_soup)) # deserializare
+            comment_list = create_comment_list(comment_data) # list of comments
+
+            
+            # rabbitmq list part
+            rabbitMQ_list_2.append([news_description, comment_list])
+        else:            
+            rabbitMQ_list_2.append([news_description])
+        # if i == 10:
+        #     break    
+        # i += 1
+    return rabbitMQ_list_2    
 
 def create_comment_url(news_id):
     url_string = 'https://social.adh.reperio.news/adevarul.ro/comment/content/'
     url_string = url_string + news_id
     return url_string
 
+def insert_into_queue(lst):
+    # Create a connection to RabbitMQ server
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    # Define the queue to send messages to
+    queue_name = 'news_queue'
+
+    # Convert the list to a JSON string
+    json_str = json.dumps(lst)
+
+    # Send the JSON string to the queue
+    channel.queue_declare(queue=queue_name)
+    channel.basic_publish(exchange='', routing_key=queue_name, body=json_str)
+
+    # Close the connection to RabbitMQ server
+    connection.close()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('comments', type=str, nargs='?', default=None)
+args = parser.parse_args()
+param = args.comments
+
 response = get_response_from_url('https://adevarul.ro/') # response
 soup = get_soup(response)
 html_class = get_html_class(soup) 
 list_of_news_links = get_list_of_news(soup)
-list_news_description_and_comments = get_description_of_news(list_of_news_links)
-print(list_news_description_and_comments)
+list_news_description_and_comments = get_description_of_news(list_of_news_links, param)
+insert_into_queue(list_news_description_and_comments)
